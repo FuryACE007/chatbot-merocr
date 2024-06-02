80844a2bcb0c9b55464c593f021ca0c1aead211b
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-06-02 10:38:14 +0530

    added db params

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'your_username',
-  password: 'your_password',
-  database: 'mercor_db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
 });
